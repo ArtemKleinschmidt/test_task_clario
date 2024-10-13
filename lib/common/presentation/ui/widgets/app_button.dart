974be8cc819e0c1d564cceca9354b15a5f9096f5
ArtemKleinschmidt <artem.kleinschmidt@gmail.com>
2024-10-13 19:51:07 +0300

    Fix ripple animation on sign up button

--- lib/common/presentation/ui/widgets/app_button.dart
+++ lib/common/presentation/ui/widgets/app_button.dart
@@ -3,34 +3,46 @@ import 'package:test_task_clario/common/presentation/ui/text_styles.dart';
 
 import '../colors.dart';
 
+const _borderRadius = BorderRadius.all(Radius.circular(30));
+
 class AppButton extends StatelessWidget {
   const AppButton(
     this.text, {
     super.key,
     this.onTap,
+    this.height = 48,
+    this.width = 240,
   });
 
   final VoidCallback? onTap;
   final String text;
+  final double height;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        height: 48,
-        width: 240,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            colors: [AppColors.blue1, AppColors.blue2],
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-          ),
-          borderRadius: BorderRadius.circular(30.0),
+    return Container(
+      height: height,
+      width: width,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [AppColors.blue1, AppColors.blue2],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
         ),
-        child: Text(text, style: AppTextStyles.buttonText),
+        borderRadius: _borderRadius,
       ),
+      child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+              borderRadius: _borderRadius,
+              onTap: onTap,
+              child: Container(
+                  height: height,
+                  width: width,
+                  alignment: Alignment.center,
+                  child: Text(text, style: AppTextStyles.buttonText)))),
     );
   }
 }
